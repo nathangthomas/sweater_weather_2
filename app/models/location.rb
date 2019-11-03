a735fd11df3f8e05348c4eb697acdfde2ce6aa19
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-02 19:23:46 -0600

    Add location model

---
+++ app/models/location.rb
@@ -0,0 +1,20 @@
+class Location
+  attr_reader :name, :lat, :lng, :coordinates, :city, :state, :country
+
+  def initialize(data)
+    # @id = rand(0..999)
+    # @id = nil
+    @name = data[:results][0][:formatted_address]
+    @lat = data[:results][0][:geometry][:location][:lat]
+    @lng = data[:results][0][:geometry][:location][:lng]
+    @coordinates = coordinates
+    @city = data[:results][0][:address_components][0][:long_name]
+    @state = data[:results][0][:address_components][2][:short_name]
+    @country = data[:results][0][:address_components][3][:long_name]
+  end
+
+  def coordinates
+    "#{@lat},#{@lng}"
+  end
+
+end
