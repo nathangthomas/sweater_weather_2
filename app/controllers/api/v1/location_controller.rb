d69db48aef5e0ef922900f3f635683ed14a5c147
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-03 00:38:31 -0600

    refactor location controller

--- app/controllers/api/v1/location_controller.rb
+++ app/controllers/api/v1/location_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::LocationController < ApplicationController
 
   def index
-    render json: LocationSerializer.new(location_facade.location)
+    render json: LocationSerializer.new(location_facade)
   end
 
 private
 
   def location_facade
-    LocationsFacade.new(params[:location])
+    facade = LocationsFacade.new(params[:location])
+    facade.location
   end
 end
