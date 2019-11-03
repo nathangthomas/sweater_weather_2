e26e31c51301c61dfc0b0b6c8388a55f7c1ec872
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-03 11:53:37 -0700

    Add flickr service

---
+++ app/services/google_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe GoogleService do
+  context "instance methods" do
+    context "coordinates by city and state" do
+      it "returns coordinates" do
+        stub_api_calls
+        # WebMock.allow_net_connect!
+        location_info = subject.location_by_city('denver,co')
+        expect(location_info).to be_a Hash
+        results = location_info[:results]
+        expect(results).to be_an Array
+        expect(results.count).to eq(1)
+
+        expect(results[0][:formatted_address]).to eq("Denver, CO, USA")
+        expect(results[0][:geometry][:location][:lat]).to eq(39.7392358)
+        expect(results[0][:geometry][:location][:lng]).to eq(-104.990251)
+      end
+    end
+  end
+end
