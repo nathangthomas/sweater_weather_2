1eaf752c3620addfa7860bfe3adfe9afc57c76fd
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-04 11:57:54 -0700

    got myself twisted up on what I needed to return to get weather data

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -6,60 +6,29 @@ class AntipodeFacade
     @search_location = search_location
     @antipode_name = antipode_name
     @location_name = antipode_name
+    @forecast = forecast
+    binding.pry
+  end
+
+  def forecast
+    DarkSkyService.new.forecast_by(antipode_coordinates)
   end
 
   def antipode_coordinates
     location = LocationsFacade.new('hong kong').location
-    Antipode.new(AmypodeService.new.antipode_by(location))
-    location.coordinates
+    antipode = Antipode.new(AmypodeService.new.antipode_by(location))
+    binding.pry
+    antipode.coordinates
   end
 
   def antipode_data
-    GoogleService.new.city_by_coordinates(antipode_data.coordinates)
+    GoogleService.new.city_by_coordinates(antipode_coordinates)
   end
 
   def antipode_name
-    binding.pry
-    antipode_data
+    city_info = antipode_data[:plus_code][:compound_code]
+    array = city_info.split(" ")
+    "#{array[2]}#{array[3]}"
   end
 
-  def antipode_location_info
-
-    # antipode_info = AmypodeService.new.antipode_by(@search_location)
-  end
-    # location_info = LocationsFacade.new('hong kong').location
-    #
-    # antipode_info = subject.antipode_by(location_info)
-
-    # AmypodeService.new.antipode_by(@search_location)
-    # LocationsFacade.new(@search_location).location
-  end
-
-  #
-  # def forecast
-  #   location = location(@city_state)
-  #   Forecast.new(data_for(location))
-  # end
-  #
-  # def city_image
-  #   Image.new(image_for(@city_state))
-  # end
-  # private
-  #
-  # def data_for(location)
-  #   @_data_for ||= DarkSkyService.new.forecast_by(location)
-  # end
-  #
-  # def location(location)
-  #   LocationsFacade.new(city_state).location
-  # end
-  #
-  # def image_for(city_state)
-  #   @_image_for ||= flickr_service
-  # end
-  #
-  # def flickr_service
-  #   FlickrService.new.image_of(@city_state)
-  # end
-
-# end
+end
