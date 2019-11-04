6910d22766f97ace1dff1dfba5b026ff0d58adf0
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-04 10:21:03 -0700

    Add passing amypode service spec

--- spec/services/amypode_service_spec.rb
+++ spec/services/amypode_service_spec.rb
@@ -4,19 +4,17 @@ describe AmypodeService do
   context "instance methods" do
     context "antipode(coordinates)" do
       it "returns a city's antipode coordinates" do
-        # stub_api_calls
+
         WebMock.allow_net_connect!
-        # response = File.read("./fixtures/amypode_data_hongkong.json")
+
         location_info = LocationsFacade.new('hong kong').location
 
         antipode_info = subject.antipode_by(location_info)
-        # data = subject.parsed_data
-        # location = .new(data)
 
         expect(antipode_info).to be_a Hash
 
-        # expect(forecast_info[:currently][:summary]).to eq("Clear")
-        expect(anipode_info[:data]).to
+        expect(antipode_info[:data].keys).to eq([:id, :type, :attributes])
+        expect(antipode_info[:data][:attributes].keys).to eq([:lat, :long])
       end
     end
   end
