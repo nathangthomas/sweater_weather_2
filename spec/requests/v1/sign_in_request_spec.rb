3c8c808a7c370f42a96b03c3861b9fdc5f9afd18
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-05 12:00:30 -0700

    Complete user log_in functionality

--- spec/requests/v1/sign_in_request_spec.rb
+++ spec/requests/v1/sign_in_request_spec.rb
@@ -1,19 +1,22 @@
 require "rails_helper"
 
 describe "User can sign into an account" do
-  before{allow(SecureRandom).to receive(:urlsafe_base64).and_return("Th15at0Tal1y-RAn20mk3Y")}
-  # allow(instance).to receive(:method).and_return(value)
-
-  #most likely will need to create a user for this test.
+  before{allow(SecureRandom).to receive(:urlsafe_base64).and_return("O-F3evweAYU1HlhgrGnXiA")}
 
   it "returns an api_key" do
 
-  post "/api/v1/sessions?email=whatever@exeample.com&password=password"
+  User.create!(email: 'nola@email.com', password: 'jazZhandz')
+  # allow(User).to receive(:find_by).and_return(@user)
+  body = {
+    "email": "nola@email.com",
+    "password": "jazZhandz"}
+
+  post "/api/v1/sessions", params: body.as_json
 
     expect(response).to be_successful
     parsed_data = JSON.parse(response.body, symbolize_names: true)
     expect(parsed_data[:data].count).to eq(3)
-    expect(parsed_data[:data][:attributes][:api_key]).to eq("Th15at0Tal1y-RAn20mk3Y")
+    expect(parsed_data[:data][:attributes][:api_key]).to eq("O-F3evweAYU1HlhgrGnXiA")
   end
 
   #sad path test here
--- spec/requests/v1/sign_up_request_spec.rb
+++ spec/requests/v1/sign_up_request_spec.rb
@@ -4,7 +4,11 @@ describe "User can sign up for an account" do
   before{allow(SecureRandom).to receive(:urlsafe_base64).and_return("Th15at0Tal1y-RAn20mk3Y")}
   it "returns an api_key" do
 
-    post "/api/v1/users?email=whatever@exeample.com&password=password&password_confirmation=password"
+    body = {
+    "email": "whatever@exampl.com",
+    "password": "password"
+  }
+    post "/api/v1/users", params: body.as_json
 
     expect(response).to be_successful
     parsed_data = JSON.parse(response.body, symbolize_names: true)
