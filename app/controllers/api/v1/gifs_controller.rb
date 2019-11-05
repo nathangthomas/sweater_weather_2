1cd0c2e5b4cb2ce4e4b10975aefeda72a77203f3
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-05 15:17:09 -0700

    Refactor

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,24 +1,13 @@
 class Api::V1::GifsController < ApplicationController
 
   def index
-    render json: GifsSerializer.new(get_gifs)
+    render json: GifsSerializer.new(gifs_facade)
   end
 
-  def gif_for(summary, time)
-    gif_data = GiphyService.new.gif_by(summary)
-    parsed_data = JSON.parse(gif_data.body, symbolize_names: true)
-    Gif.new(parsed_data, summary, time)
-  end
+private
 
-  def hourly_forecast
-    forecast_object = ForecastFacade.new(params[:location]).forecast
-    forecast_object.hourly
+  def gifs_facade
+    facade = GifsFacade.new(params[:location])
+    facade.get_gifs
   end
-
-  def get_gifs
-    hourly_forecast.map do |forecast|
-      gif_data = gif_for(forecast[:summary],forecast[:time])
-    end
-  end
-
 end
--- spec/models/gif_spec.rb
+++ spec/models/gif_spec.rb
@@ -3,11 +3,14 @@ require "rails_helper"
 describe Gif do
   it "exists" do
     WebMock.allow_net_connect!
+    summary = 'Mostly cloudy throughout the day'
+    time = 1572987600
 
     service = GiphyService.new
-    gif_data = service.gif_by('Mostly cloudy throughout the day')
+    gif_data = service.gif_by(summary)
     parsed_gif_data = JSON.parse(gif_data.body, symbolize_names: true)
-    gif = Gif.new(parsed_gif_data)
+
+    gif = Gif.new(parsed_gif_data, summary, time)
 
     expect(gif).to be_a Gif
     expect(gif.url).to eq("https://giphy.com/gifs/dark-castle-63xBFHKNVjZlu")
