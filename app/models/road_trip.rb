9bfd0d98664fb529c2be446b92c2731afbd23216
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-06 00:05:40 -0700

    Add road_trip model and serializer

---
+++ app/models/road_trip.rb
@@ -0,0 +1,10 @@
+class RoadTrip
+  attr_reader :id, :forecast_upon_arrival, :travel_time
+
+  def initialize(data)
+    @id = rand(0..999)
+    # @travel_time =
+    # @forecast_upon_arrival =
+  end
+  
+end
