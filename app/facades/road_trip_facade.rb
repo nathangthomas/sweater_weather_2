6d956abc085e77c02ebd48c52ad8b8ba45c86dab
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-06 00:05:53 -0700

    Add road_trip facade

---
+++ app/facades/road_trip_facade.rb
@@ -0,0 +1,48 @@
+class RoadTripFacade
+  attr_reader :id
+
+  def initialize(params)
+    @origin = params[:origin]
+    @destination = params[:destination]
+    @travel_time = travel_time_text
+    @forecast_upon_arrival = forecast_upon_arrival
+  end
+
+private
+
+  def forecast_upon_arrival
+    hourly_forecast = destination_current_forecast.hourly
+    hourly_forecast.find do |hour|
+      hour[:time] >= arrival_time
+    end
+  end
+
+  def destination_current_forecast
+    forecast_data = DarkSkyService.new.forecast_by(location)
+    Forecast.new(forecast_data)
+  end
+
+  def location
+    location_data = GoogleService.new.location_by_city(@destination)
+    Location.new(location_data)
+  end
+
+  def arrival_time
+    date_object = Time.now.utc + travel_time_value
+    date_object.to_i
+  end
+
+  def travel_time_text
+    road_trip_data[:routes][0][:legs][0][:duration][:text]
+  end
+
+  def travel_time_value
+    road_trip_data[:routes][0][:legs][0][:duration][:value]
+  end
+
+  def road_trip_data
+    service = GoogleService.new
+    service.time_to_travel_between(@origin, @destination)
+  end
+
+end
