586c09b29c82283335513d3dfec217f62509128b
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-06 00:05:21 -0700

    Refactor road_trip request spec

--- spec/requests/v1/road_trip_request_spec.rb
+++ spec/requests/v1/road_trip_request_spec.rb
@@ -4,17 +4,22 @@ describe 'Road Trip' do
   it "returns forecast for the hour of arrival" do
     WebMock.allow_net_connect!
 
+    user = User.create!(
+      email: "sampleuser@email.com",
+      password: "password"
+    )
+
     body = {
       "origin": "Denver,CO",
       "destination": "Pueblo,CO",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "api_key": "#{user.api_key}"
     }
+    
     post '/api/v1/road_trip', params: body.as_json
 
     expect(response).to be_successful
     image_data = JSON.parse(response.body, symbolize_names: true)
 
     #come back and fill in tests
-
   end
 end
